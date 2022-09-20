01ce8c5821b41e8d415b01378d8488a8343e9d3b
George Patterson <g-patt@outlook.com>
2022-09-20 15:23:10 +1000

    _app interface Props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,19 @@
+import { NextComponentType } from "next";
 import Head from "next/head";
 import '../styles/bootstrap.min.css'
 
-function MyApp({ Component, pageProps }) {
+interface Props {
+  Component: NextComponentType
+  // any props that come into the component
+}
+
+function MyApp({Component, ...props} : Props) {
     return (
         <>
           <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1" />
           </Head>
-          <Component {...pageProps} />
+          <Component/>
         </>
     );
 }
